29243a268ccdf252604dcd9b6700f2164408a554
actusnileh <actusnileh@gmail.com>
2024-09-05 09:54:49 +0300

    First Commit

---
+++ Console_Labs/Lab1.cs
@@ -0,0 +1,62 @@
+using Microsoft.VisualBasic;
+
+public class Lab1()
+{
+
+    private class Student(string numberLab, string fio, string group, string dateOfBirth, string city, string subjectSchool, string hobby)
+    {
+        private string NumberLab { get; } = numberLab;
+        private string FIO { get; } = fio;
+        private string Group { get; } = group;
+        private string DateOfBirth { get; } = dateOfBirth;
+        private string City { get; } = city;
+        private string SubjectSchool { get; } = subjectSchool;
+        private string Hobby { get; } = hobby;
+
+        public void DisplayInformation()
+        {
+            Console.WriteLine(
+                "Задание 1.\n" +
+                $"Лаб. работа: {NumberLab}\n" +
+                $"ФИО: {FIO}\n" +
+                $"Группа: {Group}\n" +
+                $"Дата рождения: {DateOfBirth}\n" +
+                $"Город: {City}\n" +
+                $"Любимый предмет в школе: {SubjectSchool}\n" +
+                $"Хобби: {Hobby}\n\n"
+            );
+        }
+    }
+
+    public static void Student_Information()
+    {
+        Student student = new(
+        "№1",
+        "Дуров Михаил Михайлович",
+        "ВМО91",
+        "19.02.1931",
+        "Rostov-on-Don",
+        "Физика",
+        "Программирование"
+        );
+
+        student.DisplayInformation();
+    }
+
+
+    public static void Calculation(double w = 12,
+        double l = 2,
+        double b = 5,
+        double t = 15,
+        double x = 31,
+        double y = 1,
+        double e = 3.12)
+
+    {
+        var s = w * l * l * l + b * t - x + y * w;
+
+        var result = s * e;
+
+        Console.WriteLine($"Задание 2.\nРезультат вычислений: {result}");
+    }
+}
---
+++ Console_Labs/Main.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Lab1.Student_Information();
+        Lab1.Calculation();
+    }
+}
